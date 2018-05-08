1e1bd1fee4ce0be06dc533898f801793c2ab14b2
Mike Tallerico <mtallerico1@gmail.com>
2018-05-07 20:47:24 -0400

    Added validation to form items

--- js/app.js
+++ js/app.js
@@ -1,9 +1,26 @@
 const fieldset = document.querySelectorAll('fieldset');
 const nameInput = document.querySelector('#name');
+const emailField = document.querySelector('#mail');
 const jobRole = document.querySelector('#title');
+const otherJob = document.querySelector('#other-title');
+const submitButton = document.querySelector('button[type="submit"]');
+const tshirtDesign = document.querySelector('#design');
+const colorLabel = document.querySelector('#colors-js-puns label');
+const colorSelection = document.querySelector('#color');
+const colorChoices = document.querySelectorAll('#color option');
+const activities = document.querySelector('.activities');
+const eventsLabel = document.querySelectorAll('.activities label');
+const eventChecks = document.querySelectorAll('.activities label input');
+const paymentMethod = document.querySelector('#payment');
+const creditCardInfo = document.querySelector('#credit-card');
+const paypalInfo = document.querySelector('#paypal');
+const bitcoinInfo = document.querySelector('#bitcoin');
+const creditCardOption = document.querySelector('#payment option[value="credit card"]');
+const emailLabel = document.querySelector('label[for="mail"]');
 const label = document.createElement('label');
 const input = document.createElement('input');
 const p = document.createElement('p');
+let total = 0;
 
 document.addEventListener('load', setFocus(nameInput));
 
@@ -24,153 +41,141 @@ function showEl(el, displayProp) {
 }
 
 
-// This function adds and input element if "other" is selected from the Job role select field.
-function addOtherJob () {
-    const otherJob = document.querySelector('#other-title');
-    hideEl(otherJob);
-    jobRole.addEventListener('change', () => {
-        if (jobRole.value === 'other') {
-            showEl(otherJob, 'block');
-            // otherJob.style.display = 'block';
-        } else {
-            hideEl(otherJob);
-        }
-    }); 
-}
-addOtherJob();
+// initially hiding div containing field for user to add job title 
+hideEl(otherJob);
+// showing and hiding job field based on user choice
+jobRole.addEventListener('change', () => {
+    if (jobRole.value === 'other') {
+        showEl(otherJob, 'block');
+    } else {
+        hideEl(otherJob);
+    }
+}); 
 
-//this function removes color options based of tshirt choice
-function setColorOptions () {
-    const tshirtDesign = document.querySelector('#design');
-    const colorLabel = document.querySelector('#colors-js-puns label');
-    const colorSelection = document.querySelector('#color');
-    const colorChoices = document.querySelectorAll('#color option');
-    hideEl(colorLabel);
-    hideEl(colorSelection);
-    tshirtDesign.addEventListener('change', () => {
-        if (tshirtDesign.value === 'js puns') {
-            showEl(colorLabel, '');
-            showEl(colorSelection, '');
-            for (let i = 0; i < colorChoices.length; i++) {
-                let color = colorChoices[i];
-                colorValue = color.value;
-                hideEl(colorChoices[i]);
-                if (colorValue === 'cornflowerblue' || colorValue === 'darkslategrey' || colorValue === 'gold' ) {
-                    showEl(colorChoices[i], '');
-                }
+// hiding color section until user selects tshirt design
+hideEl(colorLabel);
+hideEl(colorSelection);
+// giving tshirt color choices based on design chosen
+tshirtDesign.addEventListener('change', () => {
+    if (tshirtDesign.value === 'js puns') {
+        showEl(colorLabel, '');
+        showEl(colorSelection, '');
+        for (let i = 0; i < colorChoices.length; i++) {
+            let color = colorChoices[i];
+            colorValue = color.value;
+            hideEl(colorChoices[i]);
+            if (colorValue === 'cornflowerblue' || colorValue === 'darkslategrey' || colorValue === 'gold' ) {
+                showEl(colorChoices[i], '');
             }
-        }  else if (tshirtDesign.value === 'heart js') {
-            colorLabel.style.display = '';
-            colorSelection.style.display = '';
-            for (let i = 0; i < colorChoices.length; i++) {
-                let color = colorChoices[i];
-                colorValue = color.value;
-                hideEl(colorChoices[i]);
-                if (colorValue === 'tomato' || colorValue === 'steelblue' || colorValue === 'dimgrey' ) {
-                    showEl(colorChoices[i], '');
-                }
-            } 
-        } else {
-            hideEl(colorLabel);
-            hideEl(colorSelection);
         }
-    });
-}
-setColorOptions();
+    }  else if (tshirtDesign.value === 'heart js') {
+        colorLabel.style.display = '';
+        colorSelection.style.display = '';
+        for (let i = 0; i < colorChoices.length; i++) {
+            let color = colorChoices[i];
+            colorValue = color.value;
+            hideEl(colorChoices[i]);
+            if (colorValue === 'tomato' || colorValue === 'steelblue' || colorValue === 'dimgrey' ) {
+                showEl(colorChoices[i], '');
+            }
+        } 
+    } else {
+        hideEl(colorLabel);
+        hideEl(colorSelection);
+    }
+});
 
 
-
-
-
-/* a function to make sure that conflicting actitives cannot be chosen 
-as well as updates total price for all activivtes chosen by user*/ 
-function chooseActivities() {
-    const activities = document.querySelector('.activities');
-    const eventsLabel = document.querySelectorAll('.activities label');
-    const eventChecks = document.querySelectorAll('.activities label input');
-    let total = 0;
     
-    // this function disables any activity that has conflicting times.
-    function toggleConflict(choice, listCheck) {
-            const eventChecked = choice.checked;
-            const parentChosen = choice.parentElement;
-            const selectionTime = parentChosen.textContent.split(/[—,$]/)[1].trim();
-            const selectionEvent = parentChosen.textContent.split(/[—,$]/)[0].trim();
-            for (let i = 0; i < listCheck.length; i++) {
-                let conflict = listCheck[i];
-                const conflictParent = conflict.parentElement;
-                const conflictTime = conflictParent.textContent.split(/[—,$]/)[1].trim();
-                const conflictEvent =  conflictParent.textContent.split(/[—,$]/)[0].trim();
-                if ((selectionTime === conflictTime) && 
-                    (selectionEvent != conflictEvent) && 
-                    eventChecked) {
-                    conflictParent.classList.add('notAvail');
-                    conflict.disabled = true;
-                } else if ((selectionTime === conflictTime) && 
-                           (selectionEvent != conflictEvent) && 
-                           !eventChecked) {
-                    conflictParent.classList.remove('notAvail');
-                    conflict.disabled = false;
-                }
+    
+// this function disables any activity that has conflicting times.
+function toggleConflict(choice, listCheck) {
+        const eventChecked = choice.checked;
+        const parentChosen = choice.parentElement;
+        const selectionTime = parentChosen.textContent.split(/[—,$]/)[1].trim();
+        const selectionEvent = parentChosen.textContent.split(/[—,$]/)[0].trim();
+        for (let i = 0; i < listCheck.length; i++) {
+            let conflict = listCheck[i];
+            const conflictParent = conflict.parentElement;
+            const conflictTime = conflictParent.textContent.split(/[—,$]/)[1].trim();
+            const conflictEvent =  conflictParent.textContent.split(/[—,$]/)[0].trim();
+            if ((selectionTime === conflictTime) && 
+                (selectionEvent != conflictEvent) && 
+                eventChecked) {
+                conflictParent.classList.add('notAvail');
+                conflict.disabled = true;
+            } else if ((selectionTime === conflictTime) && 
+                        (selectionEvent != conflictEvent) && 
+                        !eventChecked) {
+                conflictParent.classList.remove('notAvail');
+                conflict.disabled = false;
             }
-
-    }
-    //updates total based on selections.
-    function updatePrice(chosenActivitie) {
-        const parentChosen = chosenActivitie.parentElement;
-        const isChecked = chosenActivitie.checked;
-        const price = parentChosen.textContent.split(/[$]/)[1].trim();
-        if (isChecked) {
-           total += Number(price);
-        } else if (!isChecked) {
-           total -= Number(price);
         }
-        activities.appendChild(p).innerHTML = `Total: $${total}`;
-    }
-    //listening for change to check boxes
-    activities.addEventListener('change', (e) => {
-        const eventChosen = e.target;
-        toggleConflict(eventChosen, eventChecks);
-        updatePrice(eventChosen);
-    }); 
+
 }
-chooseActivities();        
+
+//updates total based on selections.
+function updatePrice(chosenActivitie) {
+    const parentChosen = chosenActivitie.parentElement;
+    const isChecked = chosenActivitie.checked;
+    const price = parentChosen.textContent.split(/[$]/)[1].trim();
+    if (isChecked) {
+        total += Number(price);
+    } else if (!isChecked) {
+        total -= Number(price);
+    }
+    activities.appendChild(p).innerHTML = `Total: $${total}`;
+}
+
+//listening for change to check boxes
+activities.addEventListener('change', (e) => {
+    const eventChosen = e.target;
+    toggleConflict(eventChosen, eventChecks);
+    updatePrice(eventChosen);
+}); 
+
+      
 
 //displays proper fields based on payment method.
-function paymentInfo() {
-    const paymentMethod = document.querySelector('#payment');
-    const submitButton = document.querySelector('button[type="submit"]');
-    const creditCardInfo = document.querySelector('#credit-card');
-    const paypalInfo = document.querySelector('#paypal');
-    const bitcoinInfo = document.querySelector('#bitcoin');
-    const creditCardOption = document.querySelector('#payment option[value="credit card"]');
-    hideEl(paypalInfo);
-    hideEl(bitcoinInfo);
-    creditCardOption.setAttribute('selected', '');
-    paymentMethod.addEventListener('change', (e) => {
-        if (paymentMethod.value === 'select_method') {
-            submitButton.disabled = true;
-            hideEl(creditCardInfo);
-            hideEl(paypalInfo);
-            hideEl(bitcoinInfo);
-        } else if (paymentMethod.value === 'credit card') {
-            submitButton.disabled = false;
-            showEl(creditCardInfo, 'block');
-            hideEl(paypalInfo);
-            hideEl(bitcoinInfo);
-        } else if (paymentMethod.value === 'paypal') {
-            submitButton.disabled = false;
-            showEl(paypalInfo, 'block');
-            hideEl(creditCardInfo);
-            hideEl(bitcoinInfo);
-        } else if (paymentMethod.value === 'bitcoin') {
-            submitButton.disabled = false;
-            showEl(bitcoinInfo, 'block');
-            hideEl(creditCardInfo);
-            hideEl(paypalInfo);
-        }
-    });
-}
-paymentInfo();
+hideEl(paypalInfo);
+hideEl(bitcoinInfo);
+creditCardOption.setAttribute('selected', '');
+paymentMethod.addEventListener('change', (e) => {
+    if (paymentMethod.value === 'select_method') {
+        submitButton.disabled = true;
+        hideEl(creditCardInfo);
+        hideEl(paypalInfo);
+        hideEl(bitcoinInfo);
+    } else if (paymentMethod.value === 'credit card') {
+        submitButton.disabled = false;
+        showEl(creditCardInfo, 'block');
+        hideEl(paypalInfo);
+        hideEl(bitcoinInfo);
+    } else if (paymentMethod.value === 'paypal') {
+        submitButton.disabled = false;
+        showEl(paypalInfo, 'block');
+        hideEl(creditCardInfo);
+        hideEl(bitcoinInfo);
+    } else if (paymentMethod.value === 'bitcoin') {
+        submitButton.disabled = false;
+        showEl(bitcoinInfo, 'block');
+        hideEl(creditCardInfo);
+        hideEl(paypalInfo);
+    }
+});
+
+
+
+emailField.addEventListener('input', (e) => {
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/.test(emailField.value)) {
+        emailLabel.removeAttribute('style');
+        emailLabel.textContent = 'Email:';
+        emailField.setCustomValidity('');
+    } else {
+        emailLabel.style.color = 'red';                                    
+        emailLabel.textContent = 'Email: (Please Enter Valid email: example@example.com';
+        emailField.setCustomValidity('Please Enter A Valid Email');
+    }
+});
 
 
